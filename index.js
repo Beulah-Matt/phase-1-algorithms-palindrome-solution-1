e2a6bb58ae9b0016b319af9941eaf886cc600927
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-26 08:24:42 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+function reverse(word) {
+  return word.split('').reverse().join('');
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = reverse(word);
+  return word==reverseWord;
+
 }
 
 /* 
   Add your pseudocode here
+  Reverse the input string
+  call of reverse array method, after turning string into array
+  if the input string == reversed string
+    return true
+  else 
+    return false
+
 */
 
 /*
   Add written explanation of your solution here
+  My function should return true when a word is the same when read in reverse
+    /*mom reversed is equals to mom -> true
+  dog reversed equals god -> not palindrome
+  
 */
 
 // You can run `node index.js` to view these console logs
